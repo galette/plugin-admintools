a3ae4b06146a3e58f4343a0724ff367c250ab861
Johan Cwiklinski <johan@x-tnd.be>
2011-12-10 21:06:08 +0000

    Add i18n

---
+++ lang/lang_english.php
@@ -0,0 +1,42 @@
+<?php
+// This file was automatically generated on Sat Dec 10 21:01:59 2011 (GMT)
+// Don't modify it by hand, rather use the target lang from the Makefile.
+
+
+// ../templates/default/admintools.tpl:18 
+$lang['That conversion stuff will read your entire database, and write back each non UTF-8 values after conversion; this may take a while.'] = 'That conversion stuff will read your entire database, and write back each non UTF-8 values after conversion; this may take a while.';
+
+// ../templates/default/admintools.tpl:21 
+$lang['Convert encoding'] = 'Convert encoding';
+
+// ../admintools.php:80 
+$lang['Administration tools'] = 'Administration tools';
+
+// ../templates/default/admintools.tpl:17 
+$lang['You should use that function if you can see strange characters in your datas; after a manual import, or if you\'ve not used Galette\'s update scripts.'] = 'You should use that function if you can see strange characters in your datas; after a manual import, or if you\'ve not used Galette\'s update scripts.';
+
+// ../templates/default/menu.tpl:4 
+$lang['Various administrative tools'] = 'Various administrative tools';
+
+// ../admintools.php:57 
+$lang['Database should have been successfully converted to UTF-8!'] = 'Database should have been successfully converted to UTF-8!';
+
+// ../templates/default/admintools.tpl:15 
+$lang['That function is available for MySQL installations only!'] = 'That function is available for MySQL installations only!';
+
+// ../templates/default/admintools.tpl:11 
+$lang['Make sure you\'ve done a backup of the database before using any of the following tools!'] = 'Make sure you\'ve done a backup of the database before using any of the following tools!';
+
+// ../templates/default/admintools.tpl:16 
+$lang['Converts the whole database data to UTF-8, do not change anything on the tables, just proceed data conversion.'] = 'Converts the whole database data to UTF-8, do not change anything on the tables, just proceed data conversion.';
+
+// ../templates/default/menu.tpl:4 
+$lang['Admin tools'] = 'Admin tools';
+
+// ../templates/default/admintools.tpl:9 
+$lang['Database tools'] = 'Database tools';
+
+// ../templates/default/menu.tpl:2 
+$lang['Admin Tools'] = 'Admin Tools';
+
+?>
