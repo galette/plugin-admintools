7aa56b7981a7c3e9dbd0d21cffb8ddd7cea47e25
Johan Cwiklinski <johan@x-tnd.be>
2012-11-30 20:42:38 +0100

    Add translations

--- lang/lang_english.php
+++ lang/lang_english.php
@@ -1,52 +1,61 @@
 <?php
-// This file was automatically generated on Thu Dec 15 04:42:51 2011 (GMT)
+// This file was automatically generated on Fri Nov 30 19:42:11 2012 (GMT)
 // Don't modify it by hand, rather use the target lang from the Makefile.
 
 
-// ../templates/default/menu.tpl:4 
-$lang['Various administrative tools'] = 'Various administrative tools';
-
-// ../templates/default/admintools.tpl:24 
+// ../templates/default/admintools.tpl:30 
 $lang['That conversion stuff will read your entire database, and write back each non UTF-8 values after conversion; this may take a while.'] = 'That conversion stuff will read your entire database, and write back each non UTF-8 values after conversion; this may take a while.';
+
+// ../admintools.php:66 
+$lang['Texts has been successfully reinitialized.'] = 'Texts has been successfully reinitialized.';
 
 // ../templates/default/admintools.tpl:10 
 $lang['Reinitialize emails contents'] = 'Reinitialize emails contents';
 
-// ../admintools.php:67 
-$lang['Texts has been successfully reinitialized.'] = 'Texts has been successfully reinitialized.';
-
-// ../templates/default/admintools.tpl:4 
-$lang['Content tools'] = 'Content tools';
-
-// ../templates/default/admintools.tpl:27 
-$lang['Convert encoding'] = 'Convert encoding';
-
-// ../admintools.php:93 
-$lang['Administration tools'] = 'Administration tools';
-
-// ../templates/default/admintools.tpl:23 
-$lang['You should use that function if you can see strange characters in your datas; after a manual import, or if you\'ve not used Galette\'s update scripts.'] = 'You should use that function if you can see strange characters in your datas; after a manual import, or if you\'ve not used Galette\'s update scripts.';
-
-// ../admintools.php:69 
+// ../admintools.php:68 
 $lang['An error occured reinitializing texts :('] = 'An error occured reinitializing texts :(';
 
-// ../admintools.php:58 
-$lang['Database should have been successfully converted to UTF-8!'] = 'Database should have been successfully converted to UTF-8!';
-
-// ../templates/default/admintools.tpl:21 
-$lang['That function is available for MySQL installations only!'] = 'That function is available for MySQL installations only!';
-
-// ../templates/default/admintools.tpl:6 ../templates/default/admintools.tpl:17 
-$lang['Make sure you\'ve done a backup of the database before using any of the following tools!'] = 'Make sure you\'ve done a backup of the database before using any of the following tools!';
-
-// ../templates/default/admintools.tpl:22 
-$lang['Converts the whole database data to UTF-8, do not change anything on the tables, just proceed data conversion.'] = 'Converts the whole database data to UTF-8, do not change anything on the tables, just proceed data conversion.';
+// ../admintools.php:81 
+$lang['Logins and passwords has been successfully filled (%i processed).'] = 'Logins and passwords has been successfully filled (%i processed).';
 
 // ../templates/default/menu.tpl:4 
 $lang['Admin tools'] = 'Admin tools';
 
+// ../templates/default/admintools.tpl:4 
+$lang['Content tools'] = 'Content tools';
+
 // ../templates/default/admintools.tpl:15 
+$lang['Generate values for empty logins and passwords'] = 'Generate values for empty logins and passwords';
+
+// ../templates/default/admintools.tpl:29 
+$lang['You should use that function if you can see strange characters in your datas; after a manual import, or if you\'ve not used Galette\'s update scripts.'] = 'You should use that function if you can see strange characters in your datas; after a manual import, or if you\'ve not used Galette\'s update scripts.';
+
+// ../templates/default/menu.tpl:4 
+$lang['Various administrative tools'] = 'Various administrative tools';
+
+// ../admintools.php:58 
+$lang['Database should have been successfully converted to UTF-8!'] = 'Database should have been successfully converted to UTF-8!';
+
+// ../templates/default/admintools.tpl:27 
+$lang['That function is available for MySQL installations only!'] = 'That function is available for MySQL installations only!';
+
+// ../admintools.php:107 
+$lang['Administration tools'] = 'Administration tools';
+
+// ../templates/default/admintools.tpl:6 ../templates/default/admintools.tpl:22 
+$lang['Make sure you\'ve done a backup of the database before using any of the following tools!'] = 'Make sure you\'ve done a backup of the database before using any of the following tools!';
+
+// ../templates/default/admintools.tpl:28 
+$lang['Converts the whole database data to UTF-8, do not change anything on the tables, just proceed data conversion.'] = 'Converts the whole database data to UTF-8, do not change anything on the tables, just proceed data conversion.';
+
+// ../admintools.php:84 
+$lang['An error occured filling empty logins and passwords :('] = 'An error occured filling empty logins and passwords :(';
+
+// ../templates/default/admintools.tpl:20 
 $lang['Database tools'] = 'Database tools';
+
+// ../templates/default/admintools.tpl:33 
+$lang['Convert encoding'] = 'Convert encoding';
 
 // ../templates/default/menu.tpl:2 
 $lang['Admin Tools'] = 'Admin Tools';
