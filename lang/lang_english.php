be99a7fc3a8db8ecc958fde21155b8e24fe4e149
Johan Cwiklinski <johan@x-tnd.be>
2012-12-10 23:48:50 +0100

    Translations

--- lang/lang_english.php
+++ lang/lang_english.php
@@ -1,63 +1,72 @@
 <?php
-// This file was automatically generated on Fri Nov 30 19:42:11 2012 (GMT)
+// This file was automatically generated on Mon Dec 10 22:48:31 2012 (GMT)
 // Don't modify it by hand, rather use the target lang from the Makefile.
 
 
-// ../templates/default/admintools.tpl:30 
-$lang['That conversion stuff will read your entire database, and write back each non UTF-8 values after conversion; this may take a while.'] = 'That conversion stuff will read your entire database, and write back each non UTF-8 values after conversion; this may take a while.';
-
-// ../admintools.php:66 
-$lang['Texts has been successfully reinitialized.'] = 'Texts has been successfully reinitialized.';
+// ../templates/default/admintools.tpl:6 ../templates/default/admintools.tpl:23 
+$lang['Make sure you\'ve done a backup of the database before using any of the following tools!'] = 'Make sure you\'ve done a backup of the database before using any of the following tools!';
 
 // ../templates/default/admintools.tpl:10 
 $lang['Reinitialize emails contents'] = 'Reinitialize emails contents';
 
-// ../admintools.php:68 
-$lang['An error occured reinitializing texts :('] = 'An error occured reinitializing texts :(';
+// ../templates/default/admintools.tpl:34 
+$lang['Convert encoding'] = 'Convert encoding';
 
-// ../admintools.php:81 
+// ../admintools.php:99 
 $lang['Logins and passwords has been successfully filled (%i processed).'] = 'Logins and passwords has been successfully filled (%i processed).';
+
+// ../templates/default/admintools.tpl:31 
+$lang['That conversion stuff will read your entire database, and write back each non UTF-8 values after conversion; this may take a while.'] = 'That conversion stuff will read your entire database, and write back each non UTF-8 values after conversion; this may take a while.';
+
+// ../admintools.php:86 
+$lang['An error occured reinitializing fields configuration :('] = 'An error occured reinitializing fields configuration :(';
 
 // ../templates/default/menu.tpl:4 
 $lang['Admin tools'] = 'Admin tools';
 
+// ../templates/default/admintools.tpl:11 
+$lang['Reinitialize fields configuration'] = 'Reinitialize fields configuration';
+
+// ../admintools.php:74 
+$lang['An error occured reinitializing texts :('] = 'An error occured reinitializing texts :(';
+
+// ../admintools.php:64 
+$lang['Database should have been successfully converted to UTF-8!'] = 'Database should have been successfully converted to UTF-8!';
+
+// ../templates/default/admintools.tpl:28 
+$lang['That function is available for MySQL installations only!'] = 'That function is available for MySQL installations only!';
+
+// ../templates/default/admintools.tpl:29 
+$lang['Converts the whole database data to UTF-8, do not change anything on the tables, just proceed data conversion.'] = 'Converts the whole database data to UTF-8, do not change anything on the tables, just proceed data conversion.';
+
+// ../templates/default/admintools.tpl:21 
+$lang['Database tools'] = 'Database tools';
+
+// ../admintools.php:72 
+$lang['Texts has been successfully reinitialized.'] = 'Texts has been successfully reinitialized.';
+
 // ../templates/default/admintools.tpl:4 
 $lang['Content tools'] = 'Content tools';
 
-// ../templates/default/admintools.tpl:15 
+// ../admintools.php:125 
+$lang['Administration tools'] = 'Administration tools';
+
+// ../templates/default/admintools.tpl:16 
 $lang['Generate values for empty logins and passwords'] = 'Generate values for empty logins and passwords';
 
-// ../templates/default/admintools.tpl:29 
+// ../templates/default/admintools.tpl:30 
 $lang['You should use that function if you can see strange characters in your datas; after a manual import, or if you\'ve not used Galette\'s update scripts.'] = 'You should use that function if you can see strange characters in your datas; after a manual import, or if you\'ve not used Galette\'s update scripts.';
 
 // ../templates/default/menu.tpl:4 
 $lang['Various administrative tools'] = 'Various administrative tools';
 
-// ../admintools.php:58 
-$lang['Database should have been successfully converted to UTF-8!'] = 'Database should have been successfully converted to UTF-8!';
-
-// ../templates/default/admintools.tpl:27 
-$lang['That function is available for MySQL installations only!'] = 'That function is available for MySQL installations only!';
-
-// ../admintools.php:107 
-$lang['Administration tools'] = 'Administration tools';
-
-// ../templates/default/admintools.tpl:6 ../templates/default/admintools.tpl:22 
-$lang['Make sure you\'ve done a backup of the database before using any of the following tools!'] = 'Make sure you\'ve done a backup of the database before using any of the following tools!';
-
-// ../templates/default/admintools.tpl:28 
-$lang['Converts the whole database data to UTF-8, do not change anything on the tables, just proceed data conversion.'] = 'Converts the whole database data to UTF-8, do not change anything on the tables, just proceed data conversion.';
-
 // ../admintools.php:84 
-$lang['An error occured filling empty logins and passwords :('] = 'An error occured filling empty logins and passwords :(';
-
-// ../templates/default/admintools.tpl:20 
-$lang['Database tools'] = 'Database tools';
-
-// ../templates/default/admintools.tpl:33 
-$lang['Convert encoding'] = 'Convert encoding';
+$lang['Fields configuration has been successfully reinitialized.'] = 'Fields configuration has been successfully reinitialized.';
 
 // ../templates/default/menu.tpl:2 
 $lang['Admin Tools'] = 'Admin Tools';
+
+// ../admintools.php:102 
+$lang['An error occured filling empty logins and passwords :('] = 'An error occured filling empty logins and passwords :(';
 
 ?>
--- lang/lang_french.php
+++ lang/lang_french.php
@@ -1,63 +1,72 @@
 <?php
-// This file was automatically generated on Fri Nov 30 19:42:11 2012 (GMT)
+// This file was automatically generated on Mon Dec 10 22:48:31 2012 (GMT)
 // Don't modify it by hand, rather use the target lang from the Makefile.
 
 
-// ../templates/default/admintools.tpl:30 
-$lang['That conversion stuff will read your entire database, and write back each non UTF-8 values after conversion; this may take a while.'] = 'Cette conversion va lire l\'intégralité de votre base de données, et ré-écrire chaque valeur non UTF-8 après conversion. Cela peut prendre un certain temps.';
-
-// ../admintools.php:66 
-$lang['Texts has been successfully reinitialized.'] = 'Les textes ont été réinitialisés avec succès.';
+// ../templates/default/admintools.tpl:6 ../templates/default/admintools.tpl:23 
+$lang['Make sure you\'ve done a backup of the database before using any of the following tools!'] = 'Assurez-vous de faire une sauvegarde de votre base de données avant d\'utiliser l\'un des outils suivants&nbsp;!';
 
 // ../templates/default/admintools.tpl:10 
 $lang['Reinitialize emails contents'] = 'Réinitialiser les contenus des emails';
 
-// ../admintools.php:68 
-$lang['An error occured reinitializing texts :('] = 'Une erreur est survenue lors de la réinitialisation des textes :(';
+// ../templates/default/admintools.tpl:34 
+$lang['Convert encoding'] = 'Conversion de l\'encodage';
 
-// ../admintools.php:81 
+// ../admintools.php:99 
 $lang['Logins and passwords has been successfully filled (%i processed).'] = 'Les login et mot de passe ont été correctement remplis (%i traités).';
+
+// ../templates/default/admintools.tpl:31 
+$lang['That conversion stuff will read your entire database, and write back each non UTF-8 values after conversion; this may take a while.'] = 'Cette conversion va lire l\'intégralité de votre base de données, et ré-écrire chaque valeur non UTF-8 après conversion. Cela peut prendre un certain temps.';
+
+// ../admintools.php:86 
+$lang['An error occured reinitializing fields configuration :('] = 'Une erreur est survenue lors de la réinitialisation de la configuration des champs :(';
 
 // ../templates/default/menu.tpl:4 
 $lang['Admin tools'] = 'Outils admin';
 
+// ../templates/default/admintools.tpl:11 
+$lang['Reinitialize fields configuration'] = 'Réinitialiser la configuration des champs';
+
+// ../admintools.php:74 
+$lang['An error occured reinitializing texts :('] = 'Une erreur est survenue lors de la réinitialisation des textes :(';
+
+// ../admintools.php:64 
+$lang['Database should have been successfully converted to UTF-8!'] = 'La base de données devrait avoir été convertie en UTF-8&nbsp;!';
+
+// ../templates/default/admintools.tpl:28 
+$lang['That function is available for MySQL installations only!'] = 'Cette fonction n\'est disponible que pour les installations sous MySQL&nbsp;!';
+
+// ../templates/default/admintools.tpl:29 
+$lang['Converts the whole database data to UTF-8, do not change anything on the tables, just proceed data conversion.'] = 'Converti l\'ensemble des données de la base  en UTF-8, ne change rien sur les tables, effectue juste la conversion.';
+
+// ../templates/default/admintools.tpl:21 
+$lang['Database tools'] = 'Outils de base de données';
+
+// ../admintools.php:72 
+$lang['Texts has been successfully reinitialized.'] = 'Les textes ont été réinitialisés avec succès.';
+
 // ../templates/default/admintools.tpl:4 
 $lang['Content tools'] = 'Outils de contenus';
 
-// ../templates/default/admintools.tpl:15 
+// ../admintools.php:125 
+$lang['Administration tools'] = 'Outils d\'administration';
+
+// ../templates/default/admintools.tpl:16 
 $lang['Generate values for empty logins and passwords'] = 'Génère des valeurs pour les login et mot de passe vides';
 
-// ../templates/default/admintools.tpl:29 
+// ../templates/default/admintools.tpl:30 
 $lang['You should use that function if you can see strange characters in your datas; after a manual import, or if you\'ve not used Galette\'s update scripts.'] = 'Vous devriez utiliser cette fonctions si d\'étranges caractères aparaissent dans vos données ; après un import manuel, ou si vous n\'avez pas utilisé les scripts de mise à jour de Galette.';
 
 // ../templates/default/menu.tpl:4 
 $lang['Various administrative tools'] = 'Divers outils administratifs';
 
-// ../admintools.php:58 
-$lang['Database should have been successfully converted to UTF-8!'] = 'La base de données devrait avoir été convertie en UTF-8&nbsp;!';
-
-// ../templates/default/admintools.tpl:27 
-$lang['That function is available for MySQL installations only!'] = 'Cette fonction n\'est disponible que pour les installations sous MySQL&nbsp;!';
-
-// ../admintools.php:107 
-$lang['Administration tools'] = 'Outils d\'administration';
-
-// ../templates/default/admintools.tpl:6 ../templates/default/admintools.tpl:22 
-$lang['Make sure you\'ve done a backup of the database before using any of the following tools!'] = 'Assurez-vous de faire une sauvegarde de votre base de données avant d\'utiliser l\'un des outils suivants&nbsp;!';
-
-// ../templates/default/admintools.tpl:28 
-$lang['Converts the whole database data to UTF-8, do not change anything on the tables, just proceed data conversion.'] = 'Converti l\'ensemble des données de la base  en UTF-8, ne change rien sur les tables, effectue juste la conversion.';
-
 // ../admintools.php:84 
-$lang['An error occured filling empty logins and passwords :('] = 'Une erreur est survenue lors de la création de valeurs pour les identifiants vides :(';
-
-// ../templates/default/admintools.tpl:20 
-$lang['Database tools'] = 'Outils de base de données';
-
-// ../templates/default/admintools.tpl:33 
-$lang['Convert encoding'] = 'Conversion de l\'encodage';
+$lang['Fields configuration has been successfully reinitialized.'] = 'La configuration du champs a été réinitialisée avec succès.';
 
 // ../templates/default/menu.tpl:2 
 $lang['Admin Tools'] = 'Outils Admin';
+
+// ../admintools.php:102 
+$lang['An error occured filling empty logins and passwords :('] = 'Une erreur est survenue lors de la création de valeurs pour les identifiants vides :(';
 
 ?>
